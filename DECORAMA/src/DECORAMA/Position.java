761ed4b7b9e89b99a06359822b4a3bbf51db4701
Sonia <sonianiaso.96@gmail.com>
2020-02-06 18:54:00 +0100

    first commit decorama

---
+++ DECORAMA/src/DECORAMA/Position.java
@@ -0,0 +1,14 @@
+package DECORAMA;
+
+public class Position {
+
+	public Position() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
